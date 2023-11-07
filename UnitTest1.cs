42c86388006dc3ead03030321294392c737b6870
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-07 12:44:15 -0400

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,69 @@
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void IsPasswordSecure_returns_false_if_password_has_less_than_8_characters()
+        {
+            var registerViewModel = new RegisterViewModel();
+
+            bool result = registerViewModel.IsPasswordSecure("1234567");
+
+            Assert.False(result);
+
+        }
+        [Fact]
+        public void IsPasswordSecure_returns_false_if_password_does_not_contains_uppercase_character()
+        {
+            var registerViewModel = new RegisterViewModel();
+
+            bool result = registerViewModel.IsPasswordSecure("12345678a");
+
+            Assert.False(result);
+
+        }
+        [Fact]
+        public void IsPasswordSecure_returns_true_if_password_contains_an_uppercase_character_and_a_symbol()
+        {
+            var registerViewModel = new RegisterViewModel();
+
+            bool result = registerViewModel.IsPasswordSecure("Q23D@45AAAAA");
+
+            Assert.True(result);
+        }
+    }
+
+    internal class RegisterViewModel
+    {
+        internal bool IsPasswordSecure(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            if (!ContieneMayuscula(password))
+            {
+                return false;
+            }
+
+            if (!ContieneSimbolo(password))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ContieneMayuscula(string password)
+        {
+            return password.Any(c => Char.IsLetter(c) && Char.IsUpper(c));
+        }
+
+        private bool ContieneSimbolo(string password)
+        {
+            return password.Any(c => Char.IsSymbol(c) || !Char.IsLetterOrDigit(c) && !Char.IsWhiteSpace(c));
+        }
+
+    }
+}
